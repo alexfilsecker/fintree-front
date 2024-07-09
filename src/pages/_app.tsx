6c831db885f60598ec86ac1b39211c1513bd0acf
Alex <alexfilsecker@gmail.com>
2024-07-09 13:25:36 +1000

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material";
+import theme from "@/style/theme";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+import Login from "@/components/login";
+
+const Home = () => {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+};
+
+export default Home;
