eeb51ef761159fed87bfdb9dcd22bc66a37e4dfb
Alex <alexfilsecker@gmail.com>
2024-07-30 12:35:17 +1000

    go to login when there is no token

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 import Home from "@/components/Home";
+import { useAppSelector } from "@/hooks/state";
 
 const HomePage = (): JSX.Element => {
+  const { userInfo } = useAppSelector((state) => state.auth);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (userInfo === null) {
+      void router.push("/");
+    }
+  }, [userInfo, router]);
+
   return <Home />;
 };
 
